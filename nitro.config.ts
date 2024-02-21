8f8abe69ba1a12a68b89c03915c5f231a6da27e4
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2024-02-21 15:14:20 -0700

    remove preset

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,11 +1,11 @@
 // https://nitro.unjs.io/config
 const dev = process.dev
 export default defineNitroConfig({
-  entry: dev ? './preset/entry.dev.ts' : undefined,
-  preset: './preset',
-  compressPublicAssets:{
-    brotli: true,
-    gzip: true
+  // entry: dev ? './preset/entry.dev.ts' : undefined,
+  // preset: './preset',
+  // compressPublicAssets:{
+  //   brotli: true,
+  //   gzip: true
   
-  }
+  // }
 })
